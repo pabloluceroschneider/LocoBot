31b987bb8daf43fa8f2cde039431534eb1ef4d82
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-08-18 21:45:18 -0300

    Agrego saludo

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const bot = new Discord.Client();
-require('dotenv').config()
+require('dotenv').config();
 
 bot.on('ready', () => {
-    console.log("LocoBot is working now!")
-})
+	console.log('LocoBot is working now!');
+});
 
-bot.on('message', msg => {
-    if(msg.content == "HELLO"){
-        msg.reply('HELLO FRIEND!');
-    }
-})
+bot.on('guildMemberAdd', (member) => {
+	const channel = member.guild.channels.cache.find((ch) => ch.name === 'bienvenidos👋');
+	if (!channel) return;
+	channel.send(`Bienvenidx a la Familia, ${member}, AHORA SOS UN LOCO`);
+});
+
+bot.on('message', (msg) => {
+	if (msg.content == 'HELLO') {
+		msg.reply('HELLO FRIEND!');
+	}
+});
 
 bot.login(process.env.TOKEN_BOT);
